3b89d169756fd84cdcd87650b47eb31ae96cb659
Asmita Dulan <24231419+aadulan@users.noreply.github.com>
2019-10-12 21:20:38 +0100

    update app.js to contain main body

--- src/App.js
+++ src/App.js
@@ -1,12 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
+import { equationGen } from './inital-data'
+import EquationSpace from './EqSpace'
+import { DragDropContext } from 'react-beautiful-dnd';
+import { Typography } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import algebra from 'algebra.js'
+import { displayExpression } from './DisplayExpression'
 
-function App() {
+
+export default function App(props) {
+    
+  const [equation, setEquation] = useState(algebra.parse(equationGen()));
+
+  // onDragStart = () => {
+      //     document.body.style.color = 'blue';
+      //     document.body.style.transition = 'background-color 0.2s ease'
+      // }
+      
+      // onDragUpdate = update => {
+          //     const { destination } = update;
+          //     const opacity = destination ? destination.index / Object.keys(this.state.tasks).length : 0 ;
+          //     document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`;
+          // }
+          
+  const onDragEnd = result => {
+      document.body.style.color = 'inherit';
+      document.body.style.backgroundColor = 'inherit';
+      const { destination, source } = result;
+
+      if (!destination) {
+          return;
+      }
+      if (destination.droppableId === source.droppableId && destination.index === source.index) {
+          return;
+      }
+      const newLhsCards = displayExpression(equation.lhs, 'lhs');
+      const newRhsCards = displayExpression(equation.rhs, 'rhs');
+
+      if(destination.droppableId !== source.droppableId){
+          var movedTask = '';
+          if(destination.droppableId === 'eqspace-rhs'){
+              movedTask = newLhsCards[source.index]
+          }else {
+              movedTask = newRhsCards[source.index]
+          }
+          const newLhs = equation.lhs.subtract(movedTask.exp)
+          const newRhs = equation.rhs.subtract(movedTask.exp)
+          var newExp = new algebra.Equation(newLhs, newRhs) 
+          setEquation(newExp)
+      
+      }
+  }
+
+
   return (
-    <div className="App">
-    </div>
+      <Grid container
+          direction="row"
+          justify="center"
+          alignItems="center"
+      >
+          <DragDropContext
+              onDragEnd={onDragEnd}
+          // onDragStart={this.onDragStart}
+          // onDragUpdate={this.onDragUpdate}
+          >
+                  <EquationSpace
+                      dropId={'eqspace-lhs'}
+                      expression={equation.lhs}
+                      side={'lhs'}
+                      />
+                      <Typography style={{margin: 10}} variant='h1'>
+                          =
+                      </Typography>
+                  <EquationSpace
+                      dropId={'eqspace-rhs'}
+                      expression={equation.rhs}
+                      side={'rhs'}
+                      />
+          </DragDropContext>
+      </Grid>
+
+
   );
 }
-
-export default App;
