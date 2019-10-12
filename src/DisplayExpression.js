fb4feb67261c411b61f40881757ea7c098ec52ac
Asmita Dulan <24231419+aadulan@users.noreply.github.com>
2019-10-12 21:19:54 +0100

    give displayExpression it's own file

---
+++ src/DisplayExpression.js
@@ -0,0 +1,17 @@
+export const displayExpression = (expression, side) => {
+    var tasks = []
+    
+    expression.terms.forEach(t => {
+        tasks.push({
+            id: `${side}-variable-${t.variables[0].variable}`, 
+            content: t.coefficients[0].numer + t.variables[0].variable, 
+            exp: t 
+        });            
+    }); 
+        expression.constants.forEach(c => {
+            tasks.push({id: `${side}-num-${c.numer}`, content:c.numer, exp: c});            
+        }); 
+
+        return tasks;
+        
+    }
