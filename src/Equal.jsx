965f57ce9a77ea79caea3a5737fdf60fb562216c
Asmita Dulan <24231419+aadulan@users.noreply.github.com>
2019-10-17 12:34:14 +0100

    remove mathjax

--- src/Equal.jsx
+++ src/Equal.jsx
@@ -2,20 +2,15 @@ import React from 'react';
 import { Typography } from '@material-ui/core';
 import { useStyles } from './Styles'
 import Card from '@material-ui/core/Card';
-// import MathJax from 'react-mathjax'
 
 export default function Equal(props){
     
     const classes = useStyles();
-    // const tex = `=`
 
     return(
         <Typography className={classes.userSelect} variant='h1'>
             <Card className={classes.equal}>
                 <span style={{ margin: 'auto' }}>=</span>
-            {/* <MathJax.Provider>
-                <MathJax.Node formula={'='}/>
-            </MathJax.Provider> */}
             </Card>
         </Typography>
 
