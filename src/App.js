b2c4928f6c3e0fed5d40b7bae4300d0ddaadd3ec
Asmita Dulan <24231419+aadulan@users.noreply.github.com>
2019-10-17 12:34:27 +0100

    fix BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import './index.css';
 import EqDisplay from './EqDisplay';
 
@@ -7,9 +7,11 @@ import EqDisplay from './EqDisplay';
 export default function App(props) {
 
   return (
-    <Router>
+    // <Router>
+      <BrowserRouter basename={window.location.pathname || ''}>
         <Route exact path="/" component={EqDisplay} />
-    </Router>
+      </BrowserRouter>
+    // </Router>
   )
 
 }
