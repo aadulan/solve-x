eb409724cc06f3c029147e04e2fa887e13006dc0
Asmita Dulan <24231419+aadulan@users.noreply.github.com>
2019-10-29 21:05:00 +0000

    change so it doesn't simplify

--- src/EqDisplay.js
+++ src/EqDisplay.js
@@ -1,40 +1,60 @@
-import React, { useState } from 'react';
-import './index.css';
-import { equationGen } from './inital-data'
-import EquationSpace from './EqSpace'
-import { DragDropContext } from 'react-beautiful-dnd';
-import Grid from '@material-ui/core/Grid';
-import algebra from 'algebra.js'
-import { displayExpression } from './DisplayExpression'
-import Equal from './Equal'
+import React, { useState } from "react";
+import "./index.css";
+import { equationGen } from "./inital-data";
+import EquationSpace from "./EqSpace";
+import { DragDropContext } from "react-beautiful-dnd";
+import Grid from "@material-ui/core/Grid";
+import algebra from "algebra.js";
+import { displayExpression } from "./DisplayExpression";
+import Equal from "./Equal";
+import { Button } from "@material-ui/core";
 
 export default function EqDisplay(props){
     const [equation, setEquation] = useState(algebra.parse(equationGen()));
 
   // onDragStart = () => {
-      //     document.body.style.color = 'blue';
-      //     document.body.style.transition = 'background-color 0.2s ease'
-      // }
-      
-      // onDragUpdate = update => {
-          //     const { destination } = update;
-          //     const opacity = destination ? destination.index / Object.keys(this.state.tasks).length : 0 ;
-          //     document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`;
-          // }
-          
-  const onDragEnd = result => {
-      document.body.style.color = 'inherit';
-      document.body.style.backgroundColor = 'inherit';
-      const { destination, source } = result;
+  //     document.body.style.color = 'blue';
+  //     document.body.style.transition = 'background-color 0.2s ease'
+  // }
 
-      if (!destination) {
-          return;
+  // onDragUpdate = update => {
+  //     const { destination } = update;
+  //     const opacity = destination ? destination.index / Object.keys(this.state.tasks).length : 0 ;
+  //     document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`;
+  // }
+
+  const onDragEnd = result => {
+    document.body.style.color = "inherit";
+    document.body.style.backgroundColor = "inherit";
+    const { destination, source } = result;
+
+    if (!destination) {
+      return;
+    }
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+    const newLhsCards = displayExpression(equation.lhs, "lhs");
+    const newRhsCards = displayExpression(equation.rhs, "rhs");
+
+    if (destination.droppableId !== source.droppableId) {
+      var movedTask = "";
+      const lhsOrigin = source.droppableId === "eqspace-lhs";
+      const rhsOrigin = source.droppableId === "eqspace-rhs";
+      if (destination.droppableId === "eqspace-rhs") {
+        movedTask = newLhsCards[source.index];
+      } else {
+        movedTask = newRhsCards[source.index];
       }
-      if (destination.droppableId === source.droppableId && destination.index === source.index) {
-          return;
-      }
-      const newLhsCards = displayExpression(equation.lhs, 'lhs');
-      const newRhsCards = displayExpression(equation.rhs, 'rhs');
+      const newLhs = equation.lhs.subtract(movedTask.exp, lhsOrigin);
+      const newRhs = equation.rhs.subtract(movedTask.exp, rhsOrigin);
+      var newExp = new algebra.Equation(newLhs, newRhs);
+      setEquation(newExp);
+    }
+  };
 
       if(destination.droppableId !== source.droppableId){
           var movedTask = '';
