c6f3b67ecb3ddf433aaee42ccbf760b29236bbcb
Asmita Dulan <24231419+aadulan@users.noreply.github.com>
2019-10-29 20:58:50 +0000

    re style equal sign

--- src/Equal.jsx
+++ src/Equal.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
 import { useStyles } from './Styles'
-import Card from '@material-ui/core/Card';
+// import Card from '@material-ui/core/Card';
+import 'katex/dist/katex.min.css';
+import TeX from '@matejmazur/react-katex';
 
 export default function Equal(props){
     
     const classes = useStyles();
 
     return(
-        <Typography className={classes.userSelect} variant='h1'>
-            <Card className={classes.equal}>
-                <span style={{ margin: 'auto' }}>=</span>
-            </Card>
+        <Typography className={classes.equal} variant='h1'>
+            {/* <Card className={classes.equal}> */}
+                <span style={{ margin: 'auto' }}>
+                    <TeX math="="/>
+                </span>
+            {/* </Card> */}
         </Typography>
 
     )
