9d6cc420250088574609e4e2c59e627096541208
Asmita Dulan <24231419+aadulan@users.noreply.github.com>
2019-10-30 20:45:15 +0000

    generate ids a different way

--- src/DisplayExpression.js
+++ src/DisplayExpression.js
@@ -1,27 +1,25 @@
 export const displayExpression = (expression, side) => {
     var tasks = []
-    
     expression.terms.forEach((t,index) => {
         var isStart  = index === 0
         var isPositive = t.coefficients[0] > 0
         tasks.push({
-            id: `${side}-variable-${t.variables[0].variable}`, 
+            id: `${side}-variable-x${index}`, 
             content:!isStart && isPositive ? "+" +  t.coefficients[0].numer + t.variables[0].variable : t.coefficients[0].numer + t.variables[0].variable, 
             exp: t 
-        });            
+        });  
     }); 
-
 
         expression.constants.forEach((c,index) => {
             var hasTerm = tasks.length !== 0  && index === 0
             var isStart  = index === 0
             var isPositive = c.numer > 0
             tasks.push({
-                id: `${side}-num-${c.numer}`, 
+                id: `${side}-num-${index}`, 
                 // content: (!isStart && isPositive) || (isStart && hasTerm && isPositive) ? "+" + c.numer : c.numer,
                 content: isPositive && (!isStart || hasTerm) ? "+" + c.numer : c.numer,  
                 exp: c
-            });            
+            });         
 
         }); 
         return tasks;
