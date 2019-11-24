a41bea0c37ff82a59aeb7de5d96ae8894a94ecd5
Asmita Dulan <24231419+aadulan@users.noreply.github.com>
2019-11-24 18:38:14 +0000

    add main sispkay as eqdisplay

---
+++ src/Calculator.js
@@ -0,0 +1,8 @@
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+        <EqDisplay/>
+      </main>
