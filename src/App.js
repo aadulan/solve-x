e3885c66ff7a60a7e1b318a43af13141ccf8cdb9
Asmita Dulan <24231419+aadulan@users.noreply.github.com>
2019-11-26 17:24:21 +0000

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
 import './index.css';
 import EqDisplay from './EqDisplay';
-import Calculator from './Calculator'
 
 
 export default function App(props) {
--- src/Calculator.js
+++ src/Calculator.js
@@ -5,22 +5,15 @@ import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import CssBaseline from '@material-ui/core/CssBaseline';
-// import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-// import ListItem from '@material-ui/core/ListItem';
-// import ListItemIcon from '@material-ui/core/ListItemIcon';
-// import ListItemText from '@material-ui/core/ListItemText';
-// import InboxIcon from '@material-ui/icons/MoveToInbox';
-// import MailIcon from '@material-ui/icons/Mail';
 import EqDisplay from './EqDisplay'
 import { Grid, CardContent, Card } from '@material-ui/core';
 import { Button, ButtonGroup } from "@material-ui/core"; 
-// import TextField from "@material-ui/core/TextField";
 
 
 const drawerWidth = 350;
