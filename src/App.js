ea71a34df68803d1e89d9c47370fdad1fde62641
Asmita Dulan <24231419+aadulan@users.noreply.github.com>
2019-12-23 17:52:23 +0000

    make start the start of the app

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
 import './index.css';
 import EqDisplay from './EqDisplay';
-
+import Start from './Start';
 
 export default function App(props) {
 
   return (
     // <Router>
       <BrowserRouter basename={window.location.pathname || ''}>
-        <Route exact path="/" component={EqDisplay} />
+        <Route exact path="/" component={Start} />
       </BrowserRouter>
     // </Router>
   )
