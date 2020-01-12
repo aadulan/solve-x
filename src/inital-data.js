5cb96e632c56768627b0ec8aba2d6cfea4dde3b3
Asmita Dulan <24231419+aadulan@users.noreply.github.com>
2020-01-12 18:03:59 +0000

    two equation types now

--- src/inital-data.js
+++ src/inital-data.js
@@ -1,22 +1,40 @@
-const initalData = {
-    equation : '2 * x - 5 = 5',
-    // tasks: [
-    //     { id: 'task-2', content: '2 * x' },
-    //     { id: 'task-4', content: '- 5' },
-    //     { id: 'task-5', content: '=' },
-    //     { id: 'task-6', content: '5' },
-    // ],
-};
+// const initalData = {
+//     equation : '2 * x - 5 = 5',
+//     // tasks: [
+//     //     { id: 'task-2', content: '2 * x' },
+//     //     { id: 'task-4', content: '- 5' },
+//     //     { id: 'task-5', content: '=' },
+//     //     { id: 'task-6', content: '5' },
+//     // ],
+// };
 
-export default initalData;
+// export default initalData;
+
+const randRange = (min, max) => (
+    Math.floor(Math.random() * (max - min + 1)) + min
+)
 
 export const equationGen = () => {
-    const randomNumber = Math.ceil(Math.random() * 10) 
-    const square = randomNumber * randomNumber
-    const randomInput = Math.ceil(Math.random() * 10)
-    const input = square - randomInput
+    const answer = Math.ceil(Math.random() * 10);
+    // const randomNumber = Math.ceil(Math.random() * 10) + 1 // between 2 and 11
+    const randomNumber = randRange(2, 11);
+    const constant = answer * randomNumber;
+    const splitConstant = randRange(2,constant);
+    const splitTerm = randRange(2,randomNumber);
 
-    return `${randomNumber} * x -${input} = ${randomInput}`
+    if (Math.random() >= 0.5){
+        return `${randomNumber + splitTerm} * x -${splitConstant} = ${splitTerm} * x + ${constant-splitConstant}`
+    }else{
+        return `${randomNumber} * x -${splitConstant} = ${constant-splitConstant}`
+    }
+        
+    // const square = randomNumber * randomNumber
+    // const randomInput = Math.ceil(Math.random() * 10)
+    // const input = square - randomInput
+
 
 }
+
+// get a random number between 1 and 10 
+// 
 
