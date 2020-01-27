3e905613e724a1ccd37c48e1e6a63af669cb169a
Asmita Dulan <24231419+aadulan@users.noreply.github.com>
2020-01-27 15:32:38 +0000

    remove comments

--- src/Start.jsx
+++ src/Start.jsx
@@ -8,26 +8,20 @@ export default function Start(props){
         <Grid container direction='column' justify='center' alignItems='center' spacing={8}>
             <Grid container item direction='row' justify='center' alignItems='center'>
                 <Link 
-                    // level="easy"
-                    // to="/solve"
                     to={{
                         pathname: "/solve",
                         state: { level: 'easy' }
                       }}
-                    // replace
                 >
                     <LevelButton image='level-1.jpg'   title='Level 1'/>
                 </Link>
             </Grid>
             <Grid container item direction='row' justify='center' alignItems='center'>
             <Link 
-                    // level="easy"
-                    // to="/solve"
                     to={{
                         pathname: "/solve",
                         state: { level: 'hard' }
                       }}
-                    // replace
                 >
                     <LevelButton image='level-2.jpg'  title='Level 2'/>
                 </Link>
