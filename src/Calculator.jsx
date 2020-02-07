9e83dc93b96ac887227e5c00e54500c155d5401a
Asmita Dulan <24231419+aadulan@users.noreply.github.com>
2020-02-07 22:44:08 +0000

    make calculator draggable

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import { Grid, CardContent, Card, CardHeader, Fab } from '@material-ui/core';
 import { Button, ButtonGroup } from "@material-ui/core";
+import Draggable from 'react-draggable';
 
 
 
@@ -29,7 +30,7 @@ export default function Calculator(props) {
     const addNumber = (e) => {
         setNumber(number.concat(e));
     }
-    
+
 
     const addSign = (e) => {
         setSign(e);
@@ -62,98 +63,102 @@ export default function Calculator(props) {
     }
 
     return (
-        <Card>
-            <CardHeader
-                title="Equation Changer"
+        <Draggable>
 
-            />
-            <CardContent>
 
-                <Grid container direction='column' justify='center' align='center'>
-                    <Grid container direction='row'>
-                        <Card style={{ width: '100%' }}>
-                            <CardContent>
-                                {/* <Grid container direction='column' justify='center' align='center'> */}
-                                <Grid container direction='row' justify='flex-start' align='center'>
-                                    <Fab style={{marginRight:20}} onClick={() => addNumberSign()} size="small" color="primary" aria-label="add">
-                                        ±
+            <Card>
+                <CardHeader
+                    title="Equation Changer"
+
+                />
+                <CardContent>
+
+                    <Grid container direction='column' justify='center' align='center'>
+                        <Grid container direction='row'>
+                            <Card style={{ width: '100%' }}>
+                                <CardContent>
+                                    {/* <Grid container direction='column' justify='center' align='center'> */}
+                                    <Grid container direction='row' justify='flex-start' align='center'>
+                                        <Fab style={{ marginRight: 20 }} onClick={() => addNumberSign()} size="small" color="primary" aria-label="add">
+                                            ±
                                         </Fab>
-                                    {/* <Grid item> */}
-                                        <Typography style={{marginRight:"auto"}} align="left" variant="h6">
+                                        {/* <Grid item> */}
+                                        <Typography style={{ marginRight: "auto" }} align="left" variant="h6">
                                             {isPositive}
                                             {number}
                                         </Typography>
+                                        {/* </Grid> */}
+                                        <Typography align="right" variant="h6">
+                                            {sign}
+                                        </Typography>
+                                    </Grid>
+
                                     {/* </Grid> */}
-                                    <Typography align="right" variant="h6">
-                                        {sign}
-                                    </Typography>
-                                </Grid>
+                                </CardContent>
+                            </Card>
+                        </Grid>
 
-                                {/* </Grid> */}
-                            </CardContent>
-                        </Card>
-                    </Grid>
+                        <Grid container direction='row'>
+                            <ButtonGroup
+                                fullWidth={true}
+                                size="large"
+                                color='primary'
+                            >
+                                <Button onClick={() => addNumber("7")}>7</Button>
+                                <Button onClick={() => addNumber("8")}>8</Button>
+                                <Button onClick={() => addNumber("9")}>9</Button>
+                                <Button onClick={() => addSign("÷")}> ÷ </Button>
+                            </ButtonGroup>
+                        </Grid>
+                        <Grid container direction='row'>
+                            <ButtonGroup
+                                fullWidth={true}
+                                size="large"
+                                color='primary'
+                            >
+                                <Button onClick={() => addNumber("4")}>4</Button>
+                                <Button onClick={() => addNumber("5")}>5</Button>
+                                <Button onClick={() => addNumber("6")}>6</Button>
+                                <Button onClick={() => addSign("×")}>×</Button>
+                            </ButtonGroup>
+                        </Grid>
+                        <Grid container direction='row'>
+                            <ButtonGroup
+                                fullWidth={true}
+                                size="large"
+                                color='primary'
+                            >
+                                <Button onClick={() => addNumber("1")}>1</Button>
+                                <Button onClick={() => addNumber("2")}>2</Button>
+                                <Button onClick={() => addNumber("3")}>3</Button>
+                                <Button onClick={() => addSign("-")}>-</Button>
+                            </ButtonGroup>
+                        </Grid>
+                        <Grid container direction='row'>
+                            <ButtonGroup
+                                fullWidth={true}
+                                size="large"
+                                color='primary'
+                            >
+                                <Button onClick={() => emptyText()} >Clear</Button>
+                                <Button value="0" onClick={() => addNumber("0")}>0</Button>
+                                <Button value="+" onClick={() => addSign("+")}>+</Button>
+                            </ButtonGroup>
 
-                    <Grid container direction='row'>
-                        <ButtonGroup
-                            fullWidth={true}
-                            size="large"
-                            color='primary'
-                        >
-                            <Button onClick={() => addNumber("7")}>7</Button>
-                            <Button onClick={() => addNumber("8")}>8</Button>
-                            <Button onClick={() => addNumber("9")}>9</Button>
-                            <Button onClick={() => addSign("÷")}> ÷ </Button>
-                        </ButtonGroup>
-                    </Grid>
-                    <Grid container direction='row'>
-                        <ButtonGroup
-                            fullWidth={true}
-                            size="large"
-                            color='primary'
-                        >
-                            <Button onClick={() => addNumber("4")}>4</Button>
-                            <Button onClick={() => addNumber("5")}>5</Button>
-                            <Button onClick={() => addNumber("6")}>6</Button>
-                            <Button onClick={() => addSign("×")}>×</Button>
-                        </ButtonGroup>
-                    </Grid>
-                    <Grid container direction='row'>
-                        <ButtonGroup
-                            fullWidth={true}
-                            size="large"
-                            color='primary'
-                        >
-                            <Button onClick={() => addNumber("1")}>1</Button>
-                            <Button onClick={() => addNumber("2")}>2</Button>
-                            <Button onClick={() => addNumber("3")}>3</Button>
-                            <Button onClick={() => addSign("-")}>-</Button>
-                        </ButtonGroup>
-                    </Grid>
-                    <Grid container direction='row'>
-                        <ButtonGroup
-                            fullWidth={true}
-                            size="large"
-                            color='primary'
-                        >
-                            <Button onClick={() => emptyText()} >Clear</Button>
-                            <Button value="0" onClick={() => addNumber("0")}>0</Button>
-                            <Button value="+" onClick={() => addSign("+")}>+</Button>
-                        </ButtonGroup>
+                        </Grid>
+                        <Grid container direction='row'>
+                            <ButtonGroup
+                                fullWidth={true}
+                                size="large"
+                                color='primary'
+                            >
+                                <Button onClick={() => calculateExpression()}>Enter</Button>
+                            </ButtonGroup>
 
+                        </Grid>
                     </Grid>
-                    <Grid container direction='row'>
-                        <ButtonGroup
-                            fullWidth={true}
-                            size="large"
-                            color='primary'
-                        >
-                            <Button onClick={() => calculateExpression()}>Enter</Button>
-                        </ButtonGroup>
-
-                    </Grid>
-                </Grid>
-            </CardContent>
-        </Card>
+                </CardContent>
+            </Card>
+        </Draggable>
     )
 }
