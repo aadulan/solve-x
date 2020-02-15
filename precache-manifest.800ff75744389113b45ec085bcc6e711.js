76c6f2fb8ed55c256b358ef082a3dfe9ecdfba3c
Asmita Dulan <24231419+aadulan@users.noreply.github.com>
2020-02-15 21:49:31 +0000

    Updates

--- precache-manifest.800ff75744389113b45ec085bcc6e711.js
+++ precache-manifest.800ff75744389113b45ec085bcc6e711.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d7e867c5bfb39be2ac417dca31cc8a1f",
+    "revision": "9c6c00795e241711346b1f588048e5bf",
     "url": "/solve-x/index.html"
   },
   {
-    "revision": "abcd907980f21a4c421e",
+    "revision": "dc2d6d1f96fdbe2a6844",
     "url": "/solve-x/static/css/2.5f31276d.chunk.css"
   },
   {
-    "revision": "3d10a3f88e24bcb14c81",
+    "revision": "fd8f7639dad3318f04a5",
     "url": "/solve-x/static/css/main.a1ff0198.chunk.css"
   },
   {
-    "revision": "abcd907980f21a4c421e",
-    "url": "/solve-x/static/js/2.86f6daf3.chunk.js"
+    "revision": "dc2d6d1f96fdbe2a6844",
+    "url": "/solve-x/static/js/2.d5938925.chunk.js"
   },
   {
-    "revision": "3d10a3f88e24bcb14c81",
-    "url": "/solve-x/static/js/main.937317f3.chunk.js"
+    "revision": "fd8f7639dad3318f04a5",
+    "url": "/solve-x/static/js/main.aae51895.chunk.js"
   },
   {
     "revision": "01adee85df7fc787a20f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/solve-x/precache-manifest.7b11c109b21c335ec339e01cf04121df.js"
+  "/solve-x/precache-manifest.800ff75744389113b45ec085bcc6e711.js"
 );
 
 self.addEventListener('message', (event) => {
