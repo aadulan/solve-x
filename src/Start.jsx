4964f34a4056df30476bf3a5ba85262664bff299
Asmita Dulan <24231419+aadulan@users.noreply.github.com>
2020-02-15 21:47:17 +0000

    condese code

--- src/Start.jsx
+++ src/Start.jsx
@@ -3,57 +3,69 @@ import { Grid } from "@material-ui/core";
 import LevelButton from './LevelButton'
 import { Link } from "react-router-dom";
 
+const levels = [{
+        title: "Positive", 
+        image: "level-1.jpg", 
+        level:"easy", 
+        freeStyle:"false",
+        name: "positive"
+    }, {
+        title: "Negative", 
+        image: "level-1.jpg", 
+        level:"easy", 
+        freeStyle:"false",
+        name: "negative"
+
+    },{
+        title: "Fractions", 
+        image: "level-2.jpg", 
+        level:"hard", 
+        freeStyle:"false",
+        name: "positive"
+
+    },{
+        title: "Negative and Fractions", 
+        image: "level-1.jpg", 
+        level:"hard", 
+        freeStyle:"false",
+        name: "negative"
+
+    },
+    {
+        title: "Free Style", 
+        image: "level-1.jpg", 
+        level:"hard", 
+        freeStyle:"true",
+        name: "negative"
+
+    }
+]
+
 export default function Start(props){
     return (
         <Grid style={{height:"100%"}} direction='column' container justify='center' alignItems='flex-start'>
             <Grid direction='column' container item alignItems='center'>
-                {/* <Image></Image> */}
-                {/* <svg src="logo.svg"></svg> */}
                 <img src="logo.svg" alt="logo" style={{width:250}} ></img>
             </Grid>
             <hr/>
             <Grid style={{marginTop:5}} direction='row' container item justify='center' alignItems='center' spacing={8}> 
-            <Grid  item>
-                <Link 
-                    to={{
-                        pathname: "/solve",
-                        state: { level: 'easy' ,
-                                freeStyle:'false'
+                {levels.map((e, index) => (
+                    <Grid  item>
+                    <Link 
+                        to={{
+                            pathname: "/solve",
+                            state: { 
+                                level: e.level,
+                                freeStyle:e.freeStyle,
+                                name: e.name
                             }
-                      }}
-                >
-                    <LevelButton image='level-1.jpg'   title='Level 1' />
-                </Link>
-            </Grid>
-            <Grid  item>
-            <Link 
-                    to={{
-                        pathname: "/solve",
-                        state: { level: 'hard',
-                                freeStyle:'false'
-                    }
-                      }}
-                >
-                    <LevelButton image='level-2.jpg'  title='Level 2'/>
-                </Link>
-            </Grid>
-            <Grid  item>
-            <Link 
-                    to={{
-                        pathname: "/solve",
-                        state: { level: 'hard',
-                                freeStyle:'true'
-                                                }
-
-                      }}
-                >
-                    <LevelButton image='level-2.jpg'  title='Free Style'/>
-                </Link>
-            </Grid>
-
+                          }}
+                    >
+                        <LevelButton image={e.image}   title={e.title} />
+                    </Link>
                 </Grid>
-
-                      
+                ))}
+                </Grid>        
         </Grid>
     )
 }
