d031ecf8100fb0f6e585706cf853461f947e4edf
Asmita Dulan <24231419+aadulan@users.noreply.github.com>
2020-02-16 20:17:14 +0000

    fix broswer router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, withRouter } from "react-router-dom";
 import './index.css';
 import EqDisplay from './EqDisplay';
 import Start from './Start';
 
+
 export default function App(props) {
 
   return (
-    // <Router>
       <BrowserRouter basename={window.location.pathname || ''}>
-        <Route exact path="/" component={Start} />
-        <Route exact path="/solve" component={EqDisplay}/>
-      </BrowserRouter>
-    // </Router>
+        <div>
+          <Route exact path="/" component={withRouter(Start)} />
+          <Route path="/solve" component={withRouter(EqDisplay)}/>
+        </div>
+     </BrowserRouter>
   )
 
 }
