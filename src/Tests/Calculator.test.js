6848f074b4b49366659ae37f4b3859e66994c345
Asmita Dulan <24231419+aadulan@users.noreply.github.com>
2021-12-16 20:14:48 +0000

    test: remove enzyme tests

--- src/Tests/Calculator.test.js
+++ src/Tests/Calculator.test.js
@@ -1,164 +1,174 @@
 import React from 'react';
-import Calculator from '../Components/Calculator/Calculator';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-// import { Button, Typography } from '@material-ui/core';
-import { createShallow } from '@material-ui/core/test-utils';
-import { act } from 'react-dom/test-utils';
-import CalculatorButton from '../Components/Calculator/CalculatorButton';
-import { Button, Typography } from '@mui/material';
+import ReactDOM from 'react-dom';
+import App from '../App';
 
-Enzyme.configure({ adapter: new Adapter() });
-
-const onCalChange = jest.fn();
-const onMessage = jest.fn();
-const onVariant = jest.fn();
-const onOpen = jest.fn();
-const onEnterChange = jest.fn();
-
-const props = {
-  onCalChange,
-  onMessage,
-  onVariant,
-  onOpen,
-  onEnterChange,
-};
-
-describe('<Calculator />', () => {
-  let shallow;
-
-  beforeAll(() => {
-    shallow = createShallow();
-  });
-  let wrapper;
-  beforeEach(() => {
-    wrapper = shallow(<Calculator {...props} />);
-  });
-
-  it('input number 1  ', () => {
-    act(() => {
-      // console.log(wrapper.debug())
-      wrapper
-        .find(CalculatorButton)
-        .find({ val: '1' })
-        .dive()
-        .find(Button)
-        .find({ id: 'cal-button-1' })
-        .simulate('click');
-    });
-    wrapper.update();
-    expect(wrapper.find(Typography).find({ className: 'number' }).text()).toBe('1');
-    expect(wrapper.find(Typography).find({ className: 'sign' }).text()).toBe('');
-  });
-
-  it('input number 11 plus  ', () => {
-    act(() => {
-      // console.log(wrapper.debug())
-      wrapper
-        .find(CalculatorButton)
-        .find({ val: '1' })
-        .dive()
-        .find(Button)
-        .find({ id: 'cal-button-1' })
-        .simulate('click');
-      wrapper
-        .find(CalculatorButton)
-        .find({ val: '1' })
-        .dive()
-        .find(Button)
-        .find({ id: 'cal-button-1' })
-        .simulate('click');
-      wrapper
-        .find(CalculatorButton)
-        .find({ val: '+' })
-        .dive()
-        .find(Button)
-        .find({ id: 'cal-button-+' })
-        .simulate('click');
-    });
-    wrapper.update();
-    expect(wrapper.find(Typography).find({ className: 'number' }).text()).toBe('11');
-    expect(wrapper.find(Typography).find({ className: 'sign' }).text()).toBe('+');
-  });
-
-  it('input number 0 - empty  ', () => {
-    act(() => {
-      // console.log(wrapper.debug())
-      wrapper
-        .find(CalculatorButton)
-        .find({ val: '0' })
-        .dive()
-        .find(Button)
-        .find({ id: 'cal-button-0' })
-        .simulate('click');
-      // wrapper.find(CalculatorButton).find({ val: '1' }).dive().find(Button).find({ id: 'cal-button-1' }).simulate('click')
-      wrapper
-        .find(CalculatorButton)
-        .find({ val: '÷' })
-        .dive()
-        .find(Button)
-        .find({ id: 'cal-button-÷' })
-        .simulate('click');
-      wrapper
-        .find(CalculatorButton)
-        .find({ val: 'cal' })
-        .dive()
-        .find(Button)
-        .find({ id: 'cal-button-cal' })
-        .simulate('click');
-    });
-    wrapper.update();
-    expect(wrapper.find(Typography).find({ className: 'number' }).text()).toBe('');
-    expect(wrapper.find(Typography).find({ className: 'sign' }).text()).toBe('');
-  });
-
-  it('input number 111111 - empty  ', () => {
-    act(() => {
-      wrapper
-        .find(CalculatorButton)
-        .find({ val: '1' })
-        .dive()
-        .find(Button)
-        .find({ id: 'cal-button-1' })
-        .simulate('click');
-      wrapper
-        .find(CalculatorButton)
-        .find({ val: '1' })
-        .dive()
-        .find(Button)
-        .find({ id: 'cal-button-1' })
-        .simulate('click');
-      wrapper
-        .find(CalculatorButton)
-        .find({ val: '1' })
-        .dive()
-        .find(Button)
-        .find({ id: 'cal-button-1' })
-        .simulate('click');
-      wrapper
-        .find(CalculatorButton)
-        .find({ val: '1' })
-        .dive()
-        .find(Button)
-        .find({ id: 'cal-button-1' })
-        .simulate('click');
-      wrapper
-        .find(CalculatorButton)
-        .find({ val: '1' })
-        .dive()
-        .find(Button)
-        .find({ id: 'cal-button-1' })
-        .simulate('click');
-      wrapper
-        .find(CalculatorButton)
-        .find({ val: '1' })
-        .dive()
-        .find(Button)
-        .find({ id: 'cal-button-1' })
-        .simulate('click');
-    });
-    wrapper.update();
-    expect(wrapper.find(Typography).find({ className: 'number' }).text()).toBe('11111');
-    // expect(wrapper.find(Typography).find({ className: 'sign' }).text()).toBe('')
-  });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
+
+// import React from 'react';
+// import Calculator from '../Components/Calculator/Calculator';
+// import Enzyme from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+// // import { Button, Typography } from '@material-ui/core';
+// import { createShallow } from '@material-ui/core/test-utils';
+// import { act } from 'react-dom/test-utils';
+// import CalculatorButton from '../Components/Calculator/CalculatorButton';
+// import { Button, Typography } from '@mui/material';
+
+// Enzyme.configure({ adapter: new Adapter() });
+
+// const onCalChange = jest.fn();
+// const onMessage = jest.fn();
+// const onVariant = jest.fn();
+// const onOpen = jest.fn();
+// const onEnterChange = jest.fn();
+
+// const props = {
+//   onCalChange,
+//   onMessage,
+//   onVariant,
+//   onOpen,
+//   onEnterChange,
+// };
+
+// describe('<Calculator />', () => {
+//   let shallow;
+
+//   beforeAll(() => {
+//     shallow = createShallow();
+//   });
+//   let wrapper;
+//   beforeEach(() => {
+//     wrapper = shallow(<Calculator {...props} />);
+//   });
+
+//   it('input number 1  ', () => {
+//     act(() => {
+//       // console.log(wrapper.debug())
+//       wrapper
+//         .find(CalculatorButton)
+//         .find({ val: '1' })
+//         .dive()
+//         .find(Button)
+//         .find({ id: 'cal-button-1' })
+//         .simulate('click');
+//     });
+//     wrapper.update();
+//     expect(wrapper.find(Typography).find({ className: 'number' }).text()).toBe('1');
+//     expect(wrapper.find(Typography).find({ className: 'sign' }).text()).toBe('');
+//   });
+
+//   it('input number 11 plus  ', () => {
+//     act(() => {
+//       // console.log(wrapper.debug())
+//       wrapper
+//         .find(CalculatorButton)
+//         .find({ val: '1' })
+//         .dive()
+//         .find(Button)
+//         .find({ id: 'cal-button-1' })
+//         .simulate('click');
+//       wrapper
+//         .find(CalculatorButton)
+//         .find({ val: '1' })
+//         .dive()
+//         .find(Button)
+//         .find({ id: 'cal-button-1' })
+//         .simulate('click');
+//       wrapper
+//         .find(CalculatorButton)
+//         .find({ val: '+' })
+//         .dive()
+//         .find(Button)
+//         .find({ id: 'cal-button-+' })
+//         .simulate('click');
+//     });
+//     wrapper.update();
+//     expect(wrapper.find(Typography).find({ className: 'number' }).text()).toBe('11');
+//     expect(wrapper.find(Typography).find({ className: 'sign' }).text()).toBe('+');
+//   });
+
+//   it('input number 0 - empty  ', () => {
+//     act(() => {
+//       // console.log(wrapper.debug())
+//       wrapper
+//         .find(CalculatorButton)
+//         .find({ val: '0' })
+//         .dive()
+//         .find(Button)
+//         .find({ id: 'cal-button-0' })
+//         .simulate('click');
+//       // wrapper.find(CalculatorButton).find({ val: '1' }).dive().find(Button).find({ id: 'cal-button-1' }).simulate('click')
+//       wrapper
+//         .find(CalculatorButton)
+//         .find({ val: '÷' })
+//         .dive()
+//         .find(Button)
+//         .find({ id: 'cal-button-÷' })
+//         .simulate('click');
+//       wrapper
+//         .find(CalculatorButton)
+//         .find({ val: 'cal' })
+//         .dive()
+//         .find(Button)
+//         .find({ id: 'cal-button-cal' })
+//         .simulate('click');
+//     });
+//     wrapper.update();
+//     expect(wrapper.find(Typography).find({ className: 'number' }).text()).toBe('');
+//     expect(wrapper.find(Typography).find({ className: 'sign' }).text()).toBe('');
+//   });
+
+//   it('input number 111111 - empty  ', () => {
+//     act(() => {
+//       wrapper
+//         .find(CalculatorButton)
+//         .find({ val: '1' })
+//         .dive()
+//         .find(Button)
+//         .find({ id: 'cal-button-1' })
+//         .simulate('click');
+//       wrapper
+//         .find(CalculatorButton)
+//         .find({ val: '1' })
+//         .dive()
+//         .find(Button)
+//         .find({ id: 'cal-button-1' })
+//         .simulate('click');
+//       wrapper
+//         .find(CalculatorButton)
+//         .find({ val: '1' })
+//         .dive()
+//         .find(Button)
+//         .find({ id: 'cal-button-1' })
+//         .simulate('click');
+//       wrapper
+//         .find(CalculatorButton)
+//         .find({ val: '1' })
+//         .dive()
+//         .find(Button)
+//         .find({ id: 'cal-button-1' })
+//         .simulate('click');
+//       wrapper
+//         .find(CalculatorButton)
+//         .find({ val: '1' })
+//         .dive()
+//         .find(Button)
+//         .find({ id: 'cal-button-1' })
+//         .simulate('click');
+//       wrapper
+//         .find(CalculatorButton)
+//         .find({ val: '1' })
+//         .dive()
+//         .find(Button)
+//         .find({ id: 'cal-button-1' })
+//         .simulate('click');
+//     });
+//     wrapper.update();
+//     expect(wrapper.find(Typography).find({ className: 'number' }).text()).toBe('11111');
+//     // expect(wrapper.find(Typography).find({ className: 'sign' }).text()).toBe('')
+//   });
+// });
--- src/Tests/Textbox.test.js
+++ src/Tests/Textbox.test.js
@@ -1,69 +1,78 @@
 import React from 'react';
-import TextBox from '../Components/TextBox/textBox';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-// import { TextField, Button } from '@material-ui/core';
-import { createShallow } from '@material-ui/core/test-utils';
-import { act } from 'react-dom/test-utils';
-import { Button, TextField } from '@mui/material';
+import ReactDOM from 'react-dom';
+import App from '../App';
 
-Enzyme.configure({ adapter: new Adapter() });
-
-const onChangeEquation = jest.fn();
-const onChangeMessage = jest.fn();
-const onChangeVariant = jest.fn();
-const onChangeOpen = jest.fn();
-
-const props = {
-  onChangeEquation,
-  onChangeMessage,
-  onChangeVariant,
-  onChangeOpen,
-};
-
-describe('<TextBox />', () => {
-  let shallow;
-
-  beforeAll(() => {
-    shallow = createShallow();
-  });
-  let wrapper;
-  beforeEach(() => {
-    wrapper = shallow(<TextBox {...props} />);
-  });
-
-  // const handleStateChange = () => {};
-
-  const goodInput = ['x = 1', '2x+ 3 = 5', '3x+ 5 = 2x+ 9', 'x+ 1 = 3', '1/2x+ 3 = 5'];
-
-  it('should empty on correct input', () => {
-    goodInput.forEach((t) => {
-      act(() => {
-        wrapper
-          .find(TextField)
-          .at(0)
-          .simulate('change', { target: { value: t } });
-        wrapper.find(Button).at(0).simulate('click');
-      });
-      wrapper.update();
-
-      expect(wrapper.find(TextField).at(0).props().value).toBe('');
-    });
-  });
-
-  const badInput = ['0', 'x^2 + 3 +4=1', '1=2', 'x', 'y', '-', 'xy=1'];
-
-  it('should error when input is wrong ', () => {
-    badInput.forEach((t) => {
-      act(() => {
-        wrapper
-          .find(TextField)
-          .at(0)
-          .simulate('change', { target: { value: t } });
-        wrapper.find(Button).at(0).simulate('click');
-      });
-      wrapper.update();
-      expect(wrapper.find(TextField).at(0).props().value).toBe(t);
-    });
-  });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
+
+// import TextBox from '../Components/TextBox/textBox';
+// import Enzyme from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+// // import { TextField, Button } from '@material-ui/core';
+// // import { createShallow } from '@material-ui/core/test-utils';
+// import { act } from 'react-dom/test-utils';
+// import { Button, TextField } from '@mui/material';
+
+// Enzyme.configure({ adapter: new Adapter() });
+
+// const onChangeEquation = jest.fn();
+// const onChangeMessage = jest.fn();
+// const onChangeVariant = jest.fn();
+// const onChangeOpen = jest.fn();
+
+// const props = {
+//   onChangeEquation,
+//   onChangeMessage,
+//   onChangeVariant,
+//   onChangeOpen,
+// };
+
+// describe('<TextBox />', () => {
+//   let shallow;
+
+//   beforeAll(() => {
+//     shallow = createShallow();
+//   });
+//   let wrapper;
+//   beforeEach(() => {
+//     wrapper = shallow(<TextBox {...props} />);
+//   });
+
+//   // const handleStateChange = () => {};
+
+//   const goodInput = ['x = 1', '2x+ 3 = 5', '3x+ 5 = 2x+ 9', 'x+ 1 = 3', '1/2x+ 3 = 5'];
+
+//   it('should empty on correct input', () => {
+//     goodInput.forEach((t) => {
+//       act(() => {
+//         wrapper
+//           .find(TextField)
+//           .at(0)
+//           .simulate('change', { target: { value: t } });
+//         wrapper.find(Button).at(0).simulate('click');
+//       });
+//       wrapper.update();
+
+//       expect(wrapper.find(TextField).at(0).props().value).toBe('');
+//     });
+//   });
+
+//   const badInput = ['0', 'x^2 + 3 +4=1', '1=2', 'x', 'y', '-', 'xy=1'];
+
+//   it('should error when input is wrong ', () => {
+//     badInput.forEach((t) => {
+//       act(() => {
+//         wrapper
+//           .find(TextField)
+//           .at(0)
+//           .simulate('change', { target: { value: t } });
+//         wrapper.find(Button).at(0).simulate('click');
+//       });
+//       wrapper.update();
+//       expect(wrapper.find(TextField).at(0).props().value).toBe(t);
+//     });
+//   });
+// });
